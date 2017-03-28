98d7adf70c28690c8b003ec87578b144a714f9f5
Oscar Valdivia <omvaldiv@gmail.com>
2017-03-27 23:19:52 -0500

    add likeyou field

--- server.js
+++ server.js
@@ -22,6 +22,72 @@ var shuffle = function(arr){
   return arr;
 }
 
+var userMatched = function(db, currentUid, uid) {
+  return new Promise(function(resolve, reject) {
+      const str = `user_matches/${currentUid}/${uid}/matched`;
+      var ref = db.ref(str);
+      console.log("ref:",str);
+      ref.once('value')
+      .then(function (snap) {
+       console.log("matched success:",snap.val());
+       resolve(snap.val()===true);
+      })
+      .catch( function(error){
+       console.log("matched failed:");
+       reject('Failed');
+      });
+  });
+}
+
+var getNextProfile = function(db, res, map,currentUid, matches){
+
+  if(Object.keys(map).length){
+
+     const userId = Object.keys(map)[0];
+
+     console.log("loop:",userId);
+
+     if (userId!=currentUid){
+
+       let obj = map[userId];
+
+       userMatched(db, currentUid, userId).then(function(matched){
+         obj.likeyou = matched;
+         matches[userId] = obj
+         delete  map[userId];
+
+         getNextProfile(db, res, map,currentUid, matches);
+
+       }).catch(function(error){
+         obj.likeyou = false;
+         matches[userId] = obj;
+         delete  map[userId];
+
+         getNextProfile(db, res, map,currentUid, matches);
+       });
+
+     }else{
+       console.log("Diff");
+       delete  map[userId];
+       getNextProfile(db, res, map,currentUid, matches);
+     }
+
+  }else{
+
+    const keys = shuffle(Object.keys(matches));
+
+     let shuffled = {};
+
+     for (var key in keys){
+        shuffled[keys[key]] = matches[keys[key]];
+     }
+
+     res.json(shuffled);
+
+
+  }
+
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -39,35 +105,15 @@ router.get('/match/:uid', function(req, res) {
     var uid = req.params.uid;
     console.log('uid:',uid);
 
-    var ref = firebase.app().database().ref('user_profiles');
+    var db = firebase.app().database();
+    var ref = db.ref('user_profiles');
     ref.once('value')
      .then(function (snap) {
-
-     var matches = {};
-     var all = snap.val();
-     for (var userId in all) {
-
-         if (all.hasOwnProperty(userId)) {
-
-           if (userId!=uid){
-
-             matches[userId] = all[userId];
-
-           }
-         }
-     }
-
-     //shuffle
-     const keys = shuffle(Object.keys(matches));
-
-     let shuffled = {};
-
-     for (var key in keys){
-        shuffled[keys[key]] = matches[keys[key]];
-     }
-
-     res.json(shuffled);
-
+       console.log("start");
+       getNextProfile(db, res, snap.val(),uid, {});
+    }).catch(function(error){
+      console.log(error);
+      res.json({});
     });
 
 });
